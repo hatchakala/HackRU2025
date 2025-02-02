7541285183f2526477efd9791d043f5b63a72892
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-02-02 00:36:03 -0500

    more change

--- rpitest.py
+++ rpitest.py
@@ -2,12 +2,12 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
-os.system("rpicam-jpeg --nopreview --timelapse 10 --output /dev/shm/image.jpg")
-
-while not os.path.exists("/dev/shm/image.jpg"):
-    pass
-
 while True:
+    os.system("rpicam-jpeg --nopreview --output /dev/shm/image.jpg")
+
+    while not os.path.exists("/dev/shm/image.jpg"):
+        pass
+
     image = cv2.imread("/dev/shm/image.jpg")
 
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
