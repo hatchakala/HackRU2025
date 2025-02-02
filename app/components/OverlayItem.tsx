50b017ea115cf312cf29042e5d2ea589feae0be7
AadityaMunjal <aadityamunjal06@gmail.com>
2025-02-01 22:03:39 -0500

    hover overlay

---
+++ app/components/OverlayItem.tsx
@@ -0,0 +1,36 @@
+import { View } from "react-native";
+import { Button } from "~/components/ui/button";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "~/components/ui/hover-card";
+import { Text } from "~/components/ui/text";
+import { LoungeStatus } from "~/types";
+
+interface OverlayItems {
+  loungeStatus: LoungeStatus;
+  x: number;
+  y: number;
+}
+
+const OverlayItem: React.FC<OverlayItems> = ({ loungeStatus, x, y }) => {
+  return (
+    <HoverCard className={`absolute z-10`} style={{ top: y, left: x }}>
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button variant="link" size="lg"></Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-fit">
+          <View>
+            {/* <Text>Lounge: {loungeStatus.loungeId}</Text> */}
+            <Text>Noise level: {loungeStatus.noiseLevel}</Text>
+            <Text>People count: {loungeStatus.peopleCount}</Text>
+          </View>
+        </HoverCardContent>
+      </HoverCard>
+    </HoverCard>
+  );
+};
+
+export default OverlayItem;
