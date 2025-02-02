96ffa4a125d8c6e88819b251d5ff6e579cd7821f
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-02-02 00:28:17 -0500

    fix bug

--- rpitest.py
+++ rpitest.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 
 os.system("rpicam-jpeg --nopreview --timelapse 10 --output /dev/shm/capture/image.jpg")
 
-# while not os.path.exists("/dev/shm/capture/image.jpg"):
+while not os.path.exists("/dev/shm/capture/image.jpg"):
+    pass
+
 image = cv2.imread("/dev/shm/capture/image.jpg")
 
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
