e2b251b8f862d10669db16385c2018b66487d54b
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-02-01 23:59:17 -0500

    stuff

--- rpitest.py
+++ rpitest.py
@@ -1,8 +1,6 @@
-from picamera import PiCamera
-import time
+import cv2
+import os
 
-camera = PiCamera()
-camera.start_preview()  # Optional: displays a preview on the screen
-time.sleep(2)  # Give the camera time to adjust
-camera.capture('/home/pi/Pictures/image.jpg')
-camera.stop_preview()  # Optional: stops the preview
+os.system("rpicam-still --nopreview --output test.jpg")
+
+cv2.imshow("Image", cv2.imread("test.jpg"))
