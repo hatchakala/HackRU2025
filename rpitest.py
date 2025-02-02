cd8ea03aad3712ce85f1dd14349ce0acec34a67d
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-02-02 00:32:27 -0500

    lets gooo

--- rpitest.py
+++ rpitest.py
@@ -7,9 +7,10 @@ os.system("rpicam-jpeg --nopreview --timelapse 10 --output /dev/shm/image.jpg")
 while not os.path.exists("/dev/shm/image.jpg"):
     pass
 
-image = cv2.imread("/dev/shm/image.jpg")
+while True:
+    image = cv2.imread("/dev/shm/image.jpg")
 
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.axis('off')  # Hide axes
-plt.show()
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.axis('off')  # Hide axes
+    plt.show()
 
