c2e8704df5b7c0133245b24e923ac6be9d70386b
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-02-02 00:52:40 -0500

    kms

--- rpitest.py
+++ rpitest.py
@@ -2,14 +2,14 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
-os.system("rpicam-jpeg --nopreview --timelapse 10 --output /dev/shm/image_%04d.jpg")
+os.system("rpicam-still --timeout 30000 --timelapse 2000 -o /dev/shm/image%04d.jpg")
 
-while not os.path.exists("/dev/shm/image.jpg"):
+while not os.path.exists("/dev/shm/image%04d.jpg"):
     pass
 
 while True:
 
-    image = cv2.imread("/dev/shm/image.jpg")
+    image = cv2.imread("/dev/shm/image%04d.jpg")
 
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.axis('off')  # Hide axes
